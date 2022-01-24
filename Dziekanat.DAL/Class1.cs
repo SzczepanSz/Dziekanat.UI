f216afe714de3a4d695bfe51cf58fbf2eae4c754
szczepos <36853128+szczepos@users.noreply.github.com>
2022-01-24 08:51:13 +0100

    naprawa nazw folderów

--- Dziekanat.DAL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Dziekanat.DAL
-{
-    public class Class1
-    {
-    }
-}
---
+++ Dziekanat.DAL/IDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Dziekanat.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Dziekanat.DAL
+{
+    public interface IDbContextFactory
+    {
+        public void SetContext(DbContextOptions<DziekanatContext> options);
+        public void ADDStudent(Student student);
+        public void DELETEStudent(int idStudent);
+        public List<Student> GetAllStudents();
+
+        public void ADDShoolSubject(Subject shoolSubjects);
+        public void DELETEShoolSubject(int idShoolSubject);
+        public List<Subject> GetAllSubject();
+
+        public void AddSomeSubjects();
+        public void AddSomeStudents();
+    }
+}
